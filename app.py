978d706353525fcc3addb4b45dcf0d280b099348
trizym <petterknutsson1@gmail.com>
2016-08-10 05:17:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -43,10 +43,8 @@ def makeWebhookResult(req):
         {
             "type": "picture",
             "picUrl": "https://github.com/trizym/nejlbot/blob/master/pictures/kram1.png?raw=true" + item +".png"
-        },
-        {
             "type": "picture",
-            "picUrl": "https://github.com/trizym/nejlbot/blob/master/pictures/IMG_1387.png?raw=true" + item +".png"
+            "picUrl": "https://github.com/trizym/nejlbot/blob/master/pictures/kram1.png?raw=true" + item +".png"
         },
         {
             "type": "text",
